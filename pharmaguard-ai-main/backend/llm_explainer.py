9ad13a3a24c2f52d92557930dcfb67129085707e
Ishika <ishikagupta9859@gmail.com>
2026-09-23 23:41:47 +0530

    Fixed syntax

--- pharmaguard-ai-main/backend/llm_explainer.py
+++ pharmaguard-ai-main/backend/llm_explainer.py
@@ -54,7 +54,7 @@ Instructions:
 
     try:
         response = client.chat.completions.create(
-            model="model="openai/gpt-oss-20b",",
+            model="openai/gpt-oss-20b",
             messages=[
                 {"role": "system", "content": "You are a clinical pharmacogenomics expert following CPIC guidelines strictly."},
                 {"role": "user", "content": prompt}
